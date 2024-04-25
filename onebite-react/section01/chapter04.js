9c2ccd2b89be5662c5d201b326403f4958e921dc
Insik Jeong <150884733+InSik-j@users.noreply.github.com>
2024-04-25 16:30:58 +0900

    섹션1 JS기본_1.4 변수와 상수

---
+++ onebite-react/section01/chapter04.js
@@ -0,0 +1,30 @@
+// 1. 변수
+let age = 27; // 변수 선언 및 초기화
+console.log(age);
+
+age = 30;
+console.log(age);
+
+// 2. 상수
+const birth = "1997.01.01"; // 초기값 설정 필수
+// birth = "123"; // 값 변경 불가
+
+// 3. 변수 명명 규칙 ( 네이밍 규칙 )
+    // 3-1. $, _ 를 제외한 기호는 사용할 수 없다.
+    let &_name;
+
+    // 3-2. 숫자로 시작할 수 없다.
+    let name1;
+    // let 2name;
+
+    // 3-3. 예약어를 사용할 수 없다.
+    // let if;
+    
+// 4. 변수 명명 가이드
+    // 이해하기 쉽고 의미가 있는 변수명 사용
+// let a = 1;              let salesCount = 1;
+// let b = 1;       ->     let refundCount = 1;
+// let c = a - b;          let totalSalesCount = salesCount - refundCount;
+
+
+
