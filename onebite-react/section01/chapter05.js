f5a8964c5521a0432bad8d6c8ed08ffffe255be8
Insik Jeong <150884733+InSik-j@users.noreply.github.com>
2024-04-25 16:42:22 +0900

    섹션1 JS기본_ 1.5 자료형

---
+++ onebite-react/section01/chapter05.js
@@ -0,0 +1,43 @@
+// 자료형
+
+
+// 원시타입 
+    // 1. Number Type
+        let num1 = 1;
+        let num2 = 1.5;
+        let num3 = -20;
+
+        // console.log(num1 + num2);
+        // console.log(num1 - num2);
+        // console.log(num1 / num2);
+        // console.log(num1 * num2);
+        // console.log(num1 % num2);
+
+        let inf = Infinity;
+        let mInf = -Infinity;
+
+        let nan = NaN;
+
+        //console.log(1 * "hello");
+
+    // 2. String Type
+        let myName = "홍길동";
+        let myLocation = "목동";
+        let introduce = myName + myLocation;
+
+        // 템플릿 리터럴 문법 - 변수의 값을 동적으로 사용 가능
+        let introduceText = `${myname}은 ${myLocation}에 거주합니다.`;
+        console.log(introduceText)
+    
+    // 3. Boolean Type
+        let inSwichOn = true;
+        let isEmpty = false;
+
+    // 4. Null Type
+        let empty = null;
+
+    // 5. Undefined Type
+        let none;
+        console.log(none);
+
+        
