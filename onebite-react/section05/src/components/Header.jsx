fe8eee616a5613f7f80e2197c3a48b98d7f8aa56
Insik Jeong <150884733+InSik-j@users.noreply.github.com>
2024-04-29 18:08:13 +0900

    섹션5 React.js 입문 5.1-5) 기본 설정 및 Props로 데이터 전송, 이벤트 처리

---
+++ onebite-react/section05/src/components/Header.jsx
@@ -0,0 +1,10 @@
+// *컴포넌트 명의 첫 글자는 대문자
+function Header(){
+    return(
+      <header>
+        <h1>header</h1>
+      </header>
+    )
+}
+
+export default Header;
---
+++ onebite-react/section05/src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+
+// App컴포넌트를 root 컴포넌트로 하는게 관례
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <App />
+)
